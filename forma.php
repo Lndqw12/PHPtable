512cf479514aabecd5dfc551a6b611df9cf44b3e
Lndqw12 <COMP222012016@mail.ru>
2016-10-26 23:34:02 +0300

    form/reg

---
+++ forma.php
@@ -0,0 +1,16 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Form</title>
+    <meta charset="Utf-8">
+</head>
+<body>
+<div>
+    <form action="registr.php" method="get">
+        <p>Майл</p><input type="text" name = "email" size="10" value="..@mail.ru"/>
+        <p>Пароль</p><input type="password" name = "password" size="10" value="1234***"/>
+		<br>
+        <input type="submit" name="go" value="Войти">
+    </form>
+</body>
+</html>
---
+++ registr.php
@@ -0,0 +1,14 @@
+﻿<meta charset="utf-8">
+<?php
+$host='localhost';
+$dbname='baza3pk';
+$user='root';
+$password='';
+	$db = new PDO("mysql:host=$host;dbname=$dbname", $email, $password);
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$db->exec("set names utf-8");
+if (isset($_POST['email'])) { $email = $_GET['email'];}
+
+if (isset($_POST['password'])) { $password = $_POST['password'];}
+    $result2 = $db->query("INSERT INTO sec (email,password) VALUES('$email','$password')");
+?>
